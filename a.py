881c3be2ad8a565896b2aaf2c053e9d618abf51c
User <lol@gmail.com>
2024-02-18 09:43:07 +0200

    l8

--- a.py
+++ a.py
@@ -2,6 +2,13 @@ import tkinter as tk
 from tkinter import *
 class Window1:
     def __init__(self,master):
+        self.master = master
+        master.title("Wimdows")
+        master.title("Window")
+        master.geometry('750X600')
+        master.iconbitmap('w.png')
+
 
 root=Tk()
+Window1(root)
 root.mainloop()
