03aa5aed4f120b3fd11f20447cc14c649535d9b9
User <lol@gmail.com>
2024-02-18 09:59:59 +0200

    lol

--- a.py
+++ a.py
@@ -1,14 +1,19 @@
 import tkinter as tk
 from tkinter import *
 class Window1:
-    def __init__(self,master):
-        self.master = master
-        master.title("Wimdows")
-        master.title("Window")
-        master.geometry('750X600')
-        master.iconbitmap('w.png')
+    def __init__(self,root):
+        self.root = root
+        root.title("Wimdows")
+        root.title("Window")
+        root.geometry('750x600')
+        root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
+        self.img =PhotoImage(file="w.png")
+        self.labelimg=Label(root,image=self.img)
+        self.labelimg.pack()
 
 
 root=Tk()
-Window1(root)
-root.mainloop()
+Window1 = Window1(root)
+
+Window1.root.mainloop()
+
