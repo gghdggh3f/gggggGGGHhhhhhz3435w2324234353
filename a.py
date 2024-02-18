5f9dad0ee950434a5341492667eb6c0da90a6b27
User <lol@gmail.com>
2024-02-18 10:45:20 +0200

    lol22

--- a.py
+++ a.py
@@ -5,19 +5,22 @@ class Window1:
         self.root = root
         root.title("Wimdows")
         root.title("Window")
-        root.geometry('399x300')
+        root.geometry('399x450')
         self.menu = tk.Menu(root)
         root.config(menu=self.menu)
         root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
         self.l1 = tk.Label(text="Cool Program")
         self.l1.pack()
         self.Menu = tk.Menu(self.menu)
-        self.menu.add_cascade(label="Hello", menu=self.Menu)
+        self.menu.add_cascade(label="Menu", menu=self.Menu)
+        self.Menu.add_command(label="Create new command Hello",command= self.lo)
         self.img =PhotoImage(file="w.png")
         self.labelimg=Label(root,image=self.img)
         self.labelimg.pack()
         self.scaleGamma = tk.Scale(root , from_=1 ,to=100,length=700,orient=HORIZONTAL,command=self.l)
         self.scaleGamma.pack()
+    def lo(self):
+        self.Menu.add_command(label="Create another command Hello", command=self.lo)
     def l(self):
         gamma = self.scaleGamma.get()
         self.img['gamma'] = gamma
