14300ea5d4eb32fd944d7546b38c3afe19ac26b7
User <lol@gmail.com>
2024-02-18 10:13:24 +0200

    lol2

--- a.py
+++ a.py
@@ -5,11 +5,17 @@ class Window1:
         self.root = root
         root.title("Wimdows")
         root.title("Window")
-        root.geometry('750x600')
+        root.geometry('399x300')
         root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
         self.img =PhotoImage(file="w.png")
         self.labelimg=Label(root,image=self.img)
         self.labelimg.pack()
+        self.scaleGamma = tk.Scale(root,from_=1,to=100,length=700,orient=HORIZONTAL,command=self.l)
+        self.scaleGamma.pack()
+    def l(self):
+        gamma = self.scaleGamma.get()
+        self.img['gamma']=gamma
+
 
 
 root=Tk()
